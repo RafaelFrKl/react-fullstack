b067303bcd89492410286905a3303dafadb0e9a2
Rafael Freire Kloecking <rafael.freire.kloecking@gmail.com>
2023-01-24 11:50:47 -0800

    university cafe feedback poll complete

--- part1/unicafe/src/Statistics.js
+++ part1/unicafe/src/Statistics.js
@@ -1,3 +1,5 @@
+import StatisticLine from "./StatisticLine"
+
 const Statistics = ({good, neutral, bad, total}) => {
     if (total === 0) {
         return (
@@ -13,12 +15,16 @@ const Statistics = ({good, neutral, bad, total}) => {
     return (
         <div>
             <h1>Statistics</h1>
-            <p>good {good}</p>
-            <p>neutral {neutral}</p>
-            <p>bad {bad}</p>
-            <p>all {total}</p>
-            <p>average {average}</p>
-            <p>positive {percentage}%</p>
+            <table>
+                <tbody>
+                    <StatisticLine text="good" value={good} />
+                    <StatisticLine text="neutral" value={neutral} />
+                    <StatisticLine text="bad" value={bad} />
+                    <StatisticLine text="all" value={total} />
+                    <StatisticLine text="average" value={average} />
+                    <StatisticLine text="positive" value={percentage + '%'} />
+                </tbody>
+            </table>
         </div>
     )
 }
