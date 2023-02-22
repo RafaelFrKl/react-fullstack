f53e7d9e183aeacbfe2888e99eaea899902f6d20
Rafael Freire Kloecking <rafael.freire.kloecking@gmail.com>
2023-02-22 13:50:05 -0800

    Add new Notes

--- part3/helloworld-node-demo/index.js
+++ part3/helloworld-node-demo/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json()) // add New Notes: HTTP POST requests
+
 let notes = [
     {
         id: 1,
@@ -19,7 +21,7 @@ let notes = [
     }
 ]
 
-// Define 4 routes to the application
+// Define 5 routes to the application
 // 1. Defines an event handler that is used to handle HTTP GET requests made to the application's / root
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
@@ -50,6 +52,34 @@ app.delete('/api/notes/:id', (request, response) => {
     notes = notes.filter(note => note.id !== id)
 
     response.status(204).end()
+})
+
+// 5. Add New Notes
+const generateId = () => { //Give a new ID
+    const maxId = notes.length > 0
+        ? Math.max(...notes.map(n => n.id))
+        : 0
+    return maxId + 1
+}
+
+app.post('/api/notes', (request, response) => {
+    const body = request.body
+
+    if (!body.content) { // content property may not be empty.
+        return response.status(400).json({
+            error: 'content missing'
+        })
+    }
+
+    const note = {
+        content: body.content,
+        important: body.important || false, // If the content property has a value, the note will be based on the received data. If the important property is missing, we will default the value to false
+        id: generateId(),
+    }
+
+    notes = notes.concat(note)
+
+    response.json(note)
 })
 
 const PORT = 3001
