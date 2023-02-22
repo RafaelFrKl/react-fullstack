32fa250109d4f6148843f90a1fd55f8348a6b58c
Rafael Freire Kloecking <rafael.freire.kloecking@gmail.com>
2023-02-22 13:08:29 -0800

    Express Web Server

--- part3/helloworld-node-demo/index.js
+++ part3/helloworld-node-demo/index.js
@@ -1,10 +1,37 @@
-const http = require('http')
+const express = require('express')
+const app = express()
 
-const app = http.createServer((request, response) => {
-    response.writeHead(200, { 'Content-Type': 'text/plain' })
-    response.end('Hello World')
+let notes = [
+    {
+        id: 1,
+        content: "HTML is easy",
+        important: true
+    },
+    {
+        id: 2,
+        content: "Browser can execute only JavaScript",
+        important: false
+    },
+    {
+        id: 3,
+        content: "GET and POST are the most important methods of HTTP protocol",
+        important: true
+    }
+]
+
+// Define two routes to the application
+// 1. Defines an event handler that is used to handle HTTP GET requests made to the application's / root
+app.get('/', (request, response) => {
+    response.send('<h1>Hello World!</h1>')
+})
+//The event handler function accepts two parameters.The first request parameter contains all of the information of the HTTP request, and the second response parameter is used to define how the request is responded to.
+
+// 2. Defines an event handler that handles HTTP GET requests made to the notes path of the application:
+app.get('/api/notes', (request, response) => {
+    response.json(notes)
 })
 
 const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
