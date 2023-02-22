07acb5d37e6102bd07e2659086316aa6bcd69a2b
Rafael Freire Kloecking <rafael.freire.kloecking@gmail.com>
2023-02-22 13:29:33 -0800

    Delete Route

--- part3/helloworld-node-demo/index.js
+++ part3/helloworld-node-demo/index.js
@@ -19,7 +19,7 @@ let notes = [
     }
 ]
 
-// Define two routes to the application
+// Define 4 routes to the application
 // 1. Defines an event handler that is used to handle HTTP GET requests made to the application's / root
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
@@ -29,6 +29,27 @@ app.get('/', (request, response) => {
 // 2. Defines an event handler that handles HTTP GET requests made to the notes path of the application:
 app.get('/api/notes', (request, response) => {
     response.json(notes)
+})
+
+// 3. Fetch an individual resource
+app.get('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    //console.log(id)
+    const note = notes.find(note => note.id === id)
+    //console.log(note)
+    if (note) {
+        response.json(note)
+    } else {
+        response.status(404).end()
+    }
+})
+
+// 4. Deletion Route
+app.delete('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    notes = notes.filter(note => note.id !== id)
+
+    response.status(204).end()
 })
 
 const PORT = 3001
