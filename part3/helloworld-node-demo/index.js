134683d29186c733a6eb6d9ec73a5b12c031dfda
Rafael Freire Kloecking <rafael.freire.kloecking@gmail.com>
2023-02-24 11:41:57 -0800

    Delete, Update, Get individ. Note, error handling

--- part3/helloworld-node-demo/index.js
+++ part3/helloworld-node-demo/index.js
@@ -4,11 +4,11 @@ const app = express()
 const cors = require('cors') // Cors Middleware
 const Note = require('./models/note') //MongoDB Model
 
+app.use(express.static('build')) //whenever express gets an HTTP GET request it will first check if the build directory contains a file corresponding to the request's address. If a correct file is found, express will return it.
 app.use(express.json()) // add New Notes: HTTP POST requests
 app.use(cors()) // Cors Middleware
-app.use(express.static('build')) //whenever express gets an HTTP GET request it will first check if the build directory contains a file corresponding to the request's address. If a correct file is found, express will return it.
 
-let notes = [
+/*let notes = [
     {
         id: 1,
         content: "HTML is easy",
@@ -24,38 +24,35 @@ let notes = [
         content: "GET and POST are the most important methods of HTTP protocol",
         important: true
     }
-]
+]*/
 
-// Define 5 routes to the application
+// Define 6 routes to the application
 // 1. Defines an event handler that is used to handle HTTP GET requests made to the application's / root
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
 })
 //The event handler function accepts two parameters.The first request parameter contains all of the information of the HTTP request, and the second response parameter is used to define how the request is responded to.
 
-// 2. Defines an event handler that handles HTTP GET requests made to the notes path of the application:
+// 2. Get all Notes - Defines an event handler that handles HTTP GET requests made to the notes path of the application:
 app.get('/api/notes', (request, response) => {
     Note.find({}).then(notes => {
         response.json(notes)
     })
 })
 
-// 3. Fetch an individual resource
-app.get('/api/notes/:id', (request, response) => {
+// 3. Fetch an individual Note
+app.get('/api/notes/:id', (request, response, next) => {
     Note.findById(request.params.id).then(note => {
-        response.json(note)
+        if (note) {
+            response.json(note)
+        } else {
+            response.status(404).end()
+        }
     })
+    .catch(error => next(error))
 })
 
-// 4. Deletion Route
-app.delete('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    notes = notes.filter(note => note.id !== id)
-
-    response.status(204).end()
-})
-
-// 5. Create New Note
+// 4. Create New Note
 app.post('/api/notes', (request, response) => {
     const body = request.body
 
@@ -73,12 +70,48 @@ app.post('/api/notes', (request, response) => {
     })
 })
 
+// 5. Update Note
+app.put('/api/notes/:id', (request, response, next) => {
+    const body = request.body
+
+    const note = {
+        content: body.content,
+        important: body.important,
+    }
+
+    Note.findByIdAndUpdate(request.params.id, note, { new: true }) //new: true parameter, causes event handler to be called with the new modified document instead of original
+        .then(updatedNote => {
+            response.json(updatedNote)
+        })
+        .catch(error => next(error))
+})
+
+// 6. Delete Note
+app.delete('/api/notes/:id', (request, response, next) => {
+    Note.findByIdAndRemove(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
+})
+
 // Middelware function for catching requests made to non-existent routes
 const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' })
 }
-
+// Handler of requests with unknown endpoint
 app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+    next(error)
+}
+// Handler of requests with result to errors - this has to be the last loaded middleware.
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
