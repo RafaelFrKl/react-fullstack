71ffc3f8c0b6c74aa20df3e739b6f01fdf2d0be2
Rafael Freire Kloecking <rafael.freire.kloecking@gmail.com>
2023-02-25 14:13:30 -0800

    Restructure files

--- part3/bloglist-backend/app.js
+++ part3/bloglist-backend/app.js
@@ -1,10 +1,15 @@
 const config = require('./utils/config')
-const http = require('http')
 const express = require('express')
 const app = express()
 const cors = require('cors')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
+require('express-async-errors')
+
+const blogsRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
+
+mongoose.set('strictQuery', false)
 
 logger.info('connecting to', config.MONGODB_URI)
 
@@ -17,25 +22,13 @@ mongoose.connect(config.MONGODB_URI)
     })
 
 app.use(cors())
+app.use(express.static('build'))
 app.use(express.json())
+app.use(middleware.requestLogger)
 
-const Blog = require('./models/blog')
-app.get('/api/blogs', (request, response) => {
-    Blog
-        .find({})
-        .then(blogs => {
-            response.json(blogs)
-        })
-})
+app.use('/api/blogs', blogsRouter)
 
-app.post('/api/blogs', (request, response) => {
-    const blog = new Blog(request.body)
-
-    blog
-        .save()
-        .then(result => {
-            response.status(201).json(result)
-        })
-})
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
