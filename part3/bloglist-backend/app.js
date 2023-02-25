4ec358777951233bc535dcaafa4d9a8f77784a51
Rafael Freire Kloecking <rafael.freire.kloecking@gmail.com>
2023-02-25 13:52:33 -0800

    separating components into app.js

---
+++ part3/bloglist-backend/app.js
@@ -0,0 +1,41 @@
+const config = require('./utils/config')
+const http = require('http')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const logger = require('./utils/logger')
+const mongoose = require('mongoose')
+
+logger.info('connecting to', config.MONGODB_URI)
+
+mongoose.connect(config.MONGODB_URI)
+    .then(() => {
+        logger.info('connected to MongoDB')
+    })
+    .catch((error) => {
+        logger.error('error connecting to MongoDB:', error.message)
+    })
+
+app.use(cors())
+app.use(express.json())
+
+const Blog = require('./models/blog')
+app.get('/api/blogs', (request, response) => {
+    Blog
+        .find({})
+        .then(blogs => {
+            response.json(blogs)
+        })
+})
+
+app.post('/api/blogs', (request, response) => {
+    const blog = new Blog(request.body)
+
+    blog
+        .save()
+        .then(result => {
+            response.status(201).json(result)
+        })
+})
+
+module.exports = app
