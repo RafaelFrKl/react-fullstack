cea22f165da541896db3affbbe544e97f8a3285d
Rafael Freire Kloecking <rafael.freire.kloecking@gmail.com>
2023-02-27 10:38:15 -0800

    Create new note with user info

--- reference/ua-notes-backend/controllers/notes.js
+++ reference/ua-notes-backend/controllers/notes.js
@@ -1,6 +1,7 @@
 // Event handlers of routes
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
+const User = require('../models/user')
 
 // Get all Notes
 notesRouter.get('/', async (request, response) => {
@@ -12,13 +13,19 @@ notesRouter.get('/', async (request, response) => {
 notesRouter.post('/', async (request, response) => {
   const body = request.body
 
+  const user = await User.findById(body.userId)
+
   const note = new Note({
     content: body.content,
     important: body.important || false,
+    user: user.id
   })
 
   const savedNote = await note.save()
-  response.status(201).json(savedNote)
+  user.notes = user.notes.concat(savedNote._id)
+  await user.save()
+
+  response.json(savedNote)
 })
 
 // Get individual Note
