7f17038296bfab2815803bc033ae6b517034449c
Rafael Freire Kloecking <rafael.freire.kloecking@gmail.com>
2023-02-26 20:57:32 -0800

    Folder restructuring

---
+++ reference/ua-notes-backend/controllers/notes.js
@@ -0,0 +1,56 @@
+// Event handlers of routes
+const notesRouter = require('express').Router()
+const Note = require('../models/note')
+
+// Get all Notes
+notesRouter.get('/', async (request, response) => {
+  const notes = await Note.find({})
+  response.json(notes)
+})
+
+// Create a new Note
+notesRouter.post('/', async (request, response) => {
+  const body = request.body
+
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+  })
+
+  const savedNote = await note.save()
+  response.status(201).json(savedNote)
+})
+
+// Get individual Note
+notesRouter.get('/:id', async (request, response) => {
+  const note = await Note.findById(request.params.id)
+  if (note) {
+    response.json(note)
+  } else {
+    response.status(404).end()
+  }
+})
+
+// Delete Note
+notesRouter.delete('/:id', async (request, response) => {
+  await Note.findByIdAndRemove(request.params.id)
+  response.status(204).end()
+})
+
+// Edit Note
+notesRouter.put('/:id', (request, response, next) => {
+  const body = request.body
+
+  const note = {
+    content: body.content,
+    important: body.important,
+  }
+
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
+})
+
+module.exports = notesRouter
