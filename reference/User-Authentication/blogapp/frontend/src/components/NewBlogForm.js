de171d80168c0aee8390ee6dcd6add0b24408007
Rafael Freire Kloecking <rafael.freire.kloecking@gmail.com>
2023-06-13 10:59:47 -0700

    Frontend complete

--- reference/User-Authentication/blogapp/frontend/src/components/NewBlogForm.js
+++ reference/User-Authentication/blogapp/frontend/src/components/NewBlogForm.js
@@ -1,51 +1,56 @@
 import { useState } from 'react'
 
-const BlogForm = ({ createBlog }) => {
+const NewBlogForm = ({ onCreate }) => {
     const [title, setTitle] = useState('')
     const [author, setAuthor] = useState('')
     const [url, setUrl] = useState('')
 
-    const handleSubmit = async (event) => {
+    const handleSubmit = (event) => {
         event.preventDefault()
-        await createBlog({ title, author, url })
+        onCreate({ title, author, url, likes: 0 })
+        setAuthor('')
+        setTitle('')
+        setUrl('')
     }
 
     return (
         <div>
-            <h4>Create a new blog</h4>
+            <h2>Create new</h2>
 
             <form onSubmit={handleSubmit}>
                 <div>
           title
                     <input
-                        id='title'
-                        placeholder='title'
                         value={title}
                         onChange={({ target }) => setTitle(target.value)}
+                        id='title'
+                        placeholder='title of the blog'
                     />
                 </div>
                 <div>
           author
                     <input
-                        id='author'
-                        placeholder='author'
                         value={author}
                         onChange={({ target }) => setAuthor(target.value)}
+                        id='author'
+                        placeholder='author of the blog'
                     />
                 </div>
                 <div>
           url
                     <input
-                        id='url'
-                        placeholder='url'
                         value={url}
                         onChange={({ target }) => setUrl(target.value)}
+                        id='url'
+                        placeholder='url of the blog'
                     />
                 </div>
-                <button type="submit">create</button>
+                <button id='create-butto' type='submit'>
+          create
+                </button>
             </form>
         </div>
     )
 }
 
-export default BlogForm
+export default NewBlogForm
