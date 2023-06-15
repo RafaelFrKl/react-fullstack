6e5d2db37e0efc09024603f38771306c757493de
Rafael Freire Kloecking <rafael.freire.kloecking@gmail.com>
2023-06-15 14:20:39 -0700

    install bootstrap

--- reference/User-Authentication/blogapp/frontend/src/index.js
+++ reference/User-Authentication/blogapp/frontend/src/index.js
@@ -1,5 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
+// Bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.min.css'
+// Bootstrap Bundle JS
+import 'bootstrap/dist/js/bootstrap.bundle.min'
+
 import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
